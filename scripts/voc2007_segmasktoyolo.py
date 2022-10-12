4351d3121d07a4a8c242895e895a365968550233
Rupal Jain <rupaljain@microsoft.com>
2022-10-12 09:59:05 +0000

    VOC dataset from segmentation masks

--- scripts/voc2007_segmasktoyolo.py
+++ scripts/voc2007_segmasktoyolo.py
@@ -1,75 +1,137 @@
-import xml.etree.ElementTree as ET
+import cv2
+import PIL.Image as Image 
+import numpy as np 
+
 import glob
 import os
 import json
 
+VOC_CLASSES = [
+    "background",
+    "aeroplane",
+    "bicycle",
+    "bird",
+    "boat",
+    "bottle",
+    "bus",
+    "car",
+    "cat",
+    "chair",
+    "cow",
+    "diningtable",
+    "dog",
+    "horse",
+    "motorbike",
+    "person",
+    "potted plant",
+    "sheep",
+    "sofa",
+    "train",
+    "tvmonitor",
+]
 
-def xml_to_yolo_bbox(bbox, w, h):
-    # xmin, ymin, xmax, ymax
-    x_center = ((bbox[2] + bbox[0]) / 2) / w
-    y_center = ((bbox[3] + bbox[1]) / 2) / h
-    width = (bbox[2] - bbox[0]) / w
-    height = (bbox[3] - bbox[1]) / h
-    return [x_center, y_center, width, height]
+
+VOC_COLORMAP = [
+    [0, 0, 0],
+    [128, 0, 0],
+    [0, 128, 0],
+    [128, 128, 0],
+    [0, 0, 128],
+    [128, 0, 128],
+    [0, 128, 128],
+    [128, 128, 128],
+    [64, 0, 0],
+    [192, 0, 0],
+    [64, 128, 0],
+    [192, 128, 0],
+    [64, 0, 128],
+    [192, 0, 128],
+    [64, 128, 128],
+    [192, 128, 128],
+    [0, 64, 0],
+    [128, 64, 0],
+    [0, 192, 0],
+    [128, 192, 0],
+    [0, 64, 128],
+]
 
 
-def yolo_to_xml_bbox(bbox, w, h):
-    # x_center, y_center width heigth
-    w_half_len = (bbox[2] * w) / 2
-    h_half_len = (bbox[3] * h) / 2
-    xmin = int((bbox[0] * w) - w_half_len)
-    ymin = int((bbox[1] * h) - h_half_len)
-    xmax = int((bbox[0] * w) + w_half_len)
-    ymax = int((bbox[1] * h) + h_half_len)
-    return [xmin, ymin, xmax, ymax]
+def _convert_to_segmentation_mask(mask):
+    # This function converts a mask from the Pascal VOC format to the format required by AutoAlbument.
+    # Pascal VOC uses an RGB image to encode the segmentation mask for that image. RGB values of a pixel
+    # encode the pixel's class.
+    height, width = mask.shape[:2]
+    segmentation_mask_dict = {}
+    for label_index, label in enumerate(VOC_COLORMAP):
+        if label_index == 0:
+            continue 
+        curr_mask = np.all(mask == label, axis=-1).astype(float)
+        if np.count_nonzero(curr_mask) > 0:
+            segmentation_mask_dict[VOC_CLASSES[label_index]] = curr_mask
+    return segmentation_mask_dict
+    
+def binarise_mask(mask_fname):
+    mask = Image.open(mask_fname)
+    mask = np.array(mask)
+    # instances are encoded as different colors
+    obj_ids = np.unique(mask)
+    # first id is the background, so remove it
+    obj_ids = obj_ids[1:]
+    # Remove the white bundary
+    boundary = obj_ids == 255
+    obj_ids = obj_ids[~boundary]
+    obj_id_to_mask = {}
+    for obj_id_index, obj_id in enumerate(obj_ids):
+        # split the color-encoded mask into a set of binary masks
+        obj_id_to_mask[obj_id] = mask == obj_ids[obj_id_index, None, None]
+    
+    # mask = cv2.imread(mask_fname)
+    # mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
+    # mask = _convert_to_segmentation_mask(mask)
+
+    return obj_id_to_mask #mask
 
 
-classes = []
-input_dir = "/home/azureuser/cloudfiles/code/Users/rupaljain/vision_datasets/VOC2007/Annotations/"
-output_dir = "/home/azureuser/cloudfiles/code/Users/rupaljain/vision_datasets/VOC2007/labels/"
-image_dir = "/home/azureuser/cloudfiles/code/Users/rupaljain/vision_datasets/VOC2007/JPEGImages/"
+if __name__ == "__main__":
+    input_dir = "/home/azureuser/cloudfiles/code/Users/rupaljain/vision_datasets/VOC2007/SegmentationClass/"
+    output_dir = "/home/azureuser/cloudfiles/code/Users/rupaljain/vision_datasets/VOC2007/labels/"
+    image_dir = "/home/azureuser/cloudfiles/code/Users/rupaljain/vision_datasets/VOC2007/JPEGImages/"
 
-# create the labels folder (output directory)
-os.makedirs(output_dir, exist_ok=True)
+    # create the labels folder (output directory)
+    os.makedirs(output_dir, exist_ok=True)
 
-# identify all the xml files in the annotations folder (input directory)
-xml_files = glob.glob(os.path.join(input_dir, '*.xml'))
-# loop through each 
-for index, xml_file in enumerate(xml_files):
-    basename = os.path.basename(xml_file)
-    filename = os.path.splitext(basename)[0]
-    # check if the label contains the corresponding image file
-    if not os.path.exists(os.path.join(image_dir, f"{filename}.jpg")):
-        print(f"{filename} image does not exist!")
-        continue
+    # identify all the xml files in the annotations folder (input directory)
+    png_files = glob.glob(os.path.join(input_dir, '*.png'))
+    # loop through each 
+    for index, png_file in enumerate(png_files):
+        basename = os.path.basename(png_file)
+        filename = os.path.splitext(basename)[0]
+        # check if the label contains the corresponding image file
+        if not os.path.exists(os.path.join(image_dir, f"{filename}.jpg")):
+            print(f"{filename} image does not exist!")
+            continue
 
-    if index % 1000 == 0:
-        print("Processing label", index)
-    result = []
+        if index % 1000 == 0:
+            print("Processing label", index)
 
-    # parse the content of the xml file
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-    width = int(root.find("size").find("width").text)
-    height = int(root.find("size").find("height").text)
+        obj_id_to_mask = binarise_mask(png_file)
+        output_list = []
+        for class_name, class_mask in obj_id_to_mask.items():
+            y_indices, x_indices = np.where(class_mask != 0)
 
-    for obj in root.findall('object'):
-        label = obj.find("name").text
-        # check for new classes and append to list
-        if label not in classes:
-            classes.append(label)
-        index = classes.index(label)
-        pil_bbox = [int(x.text) for x in obj.find("bndbox")]
-        yolo_bbox = xml_to_yolo_bbox(pil_bbox, width, height)
-        # convert data to string
-        bbox_string = " ".join([str(x) for x in yolo_bbox])
-        result.append(f"{index} {bbox_string}")
+            h, w = class_mask.shape
+            normalised_y_indices = [y_index/h for y_index in y_indices]
+            normalised_x_indices = [x_index/w for x_index in x_indices]
+            curr_mask_line = [str(class_name)]
+            for x_index, y_index in zip(normalised_x_indices, normalised_y_indices):
+                curr_mask_line.extend([str(x_index), str(y_index)])
+            
+            curr_mask_line = " ".join(curr_mask_line)
+            output_list.append(curr_mask_line)
 
-    if result:
-        # generate a YOLO format text file for each xml file
-        with open(os.path.join(output_dir, f"{filename}.txt"), "w", encoding="utf-8") as f:
-            f.write("\n".join(result))
-
-# generate the classes file as reference
-with open('classes.txt', 'w', encoding='utf8') as f:
-    f.write(json.dumps(classes))
+        if output_list:
+            # generate a YOLO format text file for each polygon file
+            save_path = os.path.join(output_dir, f"{filename}.txt")
+            # print(save_path)
+            with open(save_path, "w", encoding="utf-8") as f:
+                f.write("\n".join(output_list))
